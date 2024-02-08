4edfa743f01b5eebce820f8921fb728f93cada7e
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2024-02-08 15:08:26 +0530

    feat: session/user stored in db

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "@/lib/mongodb";
 
 const handler = NextAuth({
+    adapter: MongoDBAdapter(clientPromise),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID ?? "",
